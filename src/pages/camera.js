53277249b06de644b2bae53caac35ee3cc64f400
Stygian84 <nicholasgandhi84@gmail.com>
2023-11-22 12:52:00 +0800

    CleanUp

--- src/pages/camera.js
+++ src/pages/camera.js
@@ -24,6 +24,14 @@ function CameraContent() {
   const location = useLocation();
   const { index } = location.state || {};
 
+  let CameraRow = [];
+  let Status = ["Overall Status", "Soil pH", "Soil Moisture", "Humidity"];
+
+  for (let i = 0; i < Status.length; i++) {
+    let name = Status[i];
+    CameraRow.push(<CameraItem type={name} value="TBC" key={i} colour="#03A400"/>);
+  }
+
   return (
     <div id="content" className="content">
       {/* Box Feature Section */}
@@ -45,49 +53,7 @@ function CameraContent() {
 
       {/* Camera item Section */}
       <div id="camera-item-container">
-        <div className="camera-item">
-          <div>
-            <img src={require("../images/bluestatus.png")} alt="Status"></img>
-          </div>
-          <div className="camera-row-status">
-            <p>
-              Overall Status: <span style={{ color: "#03A400" }}>GOOD</span>
-            </p>
-          </div>
-        </div>
-
-        <div className="camera-item">
-          <div>
-            <img src={require("../images/bluesoilph.png")} alt="Status"></img>
-          </div>
-          <div className="camera-row-status">
-            <p>
-              Soil pH: <span style={{ color: "#03A400" }}>7.2</span>
-            </p>
-          </div>
-        </div>
-
-        <div className="camera-item">
-          <div>
-            <img src={require("../images/bluemoisture.png")} alt="Status"></img>
-          </div>
-          <div className="camera-row-status">
-            <p>
-              Soil Moisture: <span style={{ color: "#FF6C02" }}>72%</span>
-            </p>
-          </div>
-        </div>
-
-        <div className="camera-item">
-          <div>
-            <img src={require("../images/bluehumidity.png")} alt="Status"></img>
-          </div>
-          <div className="camera-row-status">
-            <p>
-              Humidity: <span style={{ color: "#03A400" }}>51%</span>
-            </p>
-          </div>
-        </div>
+        {CameraRow}
 
         <div className="camera-item" style={{ height: "7vh", backgroundColor: "#7AA0B8" }}>
           <div id="water-the-plant">
@@ -102,6 +68,21 @@ function CameraContent() {
             </p>
           </div>
         </div>
+      </div>
+    </div>
+  );
+}
+
+function CameraItem(props) {
+  return (
+    <div className="camera-item">
+      <div>
+        <img src={require(`../images/blue${props.type}.png`)} alt="Status"></img>
+      </div>
+      <div className="camera-row-status">
+        <p>
+          {props.type} : <span style={{ color: props.colour }}>{props.value}</span>
+        </p>
       </div>
     </div>
   );
