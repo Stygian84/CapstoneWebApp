b57043957d3aab376dfac282627aa83ec9c2b189
Stygian84 <nicholasgandhi84@gmail.com>
2024-02-18 14:02:46 +0800

    Remove Level 4

--- src/pages/level.js
+++ src/pages/level.js
@@ -38,7 +38,7 @@ function LevelContent() {
 
         const newStatus = [];
 
-        for (let i = 0; i < 3; i++) {
+        for (let i = 0; i < 2; i++) {
           newStatus.push({ status: data[i]["status"], levelid: data[i]["levelid"] });
         }
 
